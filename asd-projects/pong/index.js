ae390bf4a1183fd206a1b968748d2361fe09ae8f
LandonGamin4421 <78760115+LandonGamin4421@users.noreply.github.com>
2025-03-17 14:26:23 +0000

    PONG

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,13 +10,37 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
   
   // Game Item Objects
+  const KEY = {
+    "W": 87,
+    "S": 83,
+    "UP": 38,
+    "DOWN": 40
+  }
+  function GameItem(id, speedX, speedY){
+    var objInstance = {
+      id: id,
+      x: parseFloat($(id).css("left")),
+      y: parseFloat($(id).css("top")),
+      speedX: speedX,
+      speedY: speedY,
+      width: $(id).width(),
+      height: $(id).height()
+    }
+    return objInstance;
+  }
 
+  var leftPaddle = GameItem("#leftPaddle", 0, 0)
+  var rightPaddle = GameItem("#rightPaddle", 0, 0)
+  var ball = GameItem("#ball", (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1), (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1) )
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +51,62 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    drawGameItem(leftPaddle);
+    updateGameItem(leftPaddle);
+    drawGameItem(rightPaddle);
+    updateGameItem(rightPaddle);
+    drawGameItem(ball);
+    updateGameItem(ball);
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if(event.which === KEY.W){
+      leftPaddle.speedY = -5;
+    }
+    if(event.which === KEY.S){
+      leftPaddle.speedY = 5;
+    }
+    if(event.which === KEY.UP){
+      rightPaddle.speedY = -5;
+    }
+    if(event.which === KEY.DOWN){
+      rightPaddle.speedY = 5;
+    }
+  }
+  function handleKeyUp(event){
+    if (event.which === KEY.W || KEY.UP){
+      leftPaddle.speedY = 0;
+      rightPaddle.speedY = 0;
+    }
+    if (event.which === KEY.S || KEY.DOWN){
+      leftPaddle.speedY = 0;
+      rightPaddle.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  //Movement helpers
+  function drawGameItem(obj){
+    $(obj.id).css("left", obj.x);
+    $(obj.id).css("top", obj.y);
+  }
+  function updateGameItem(obj){
+    obj.x += obj.speedX;
+    obj.y += obj.speedY;
+  }
+
+  //check boundaries of paddles
+  //handle what happens when the ball hits the walls
+  //handle what happens when the ball hits the handles
+  //handle what happens when someone wins
+  //handle the points
+  //handle resetting the game
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
